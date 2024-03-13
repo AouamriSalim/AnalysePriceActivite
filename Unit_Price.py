8a7d9367b990d1657c2c826235751b3a0ef1737e
AouamriSalim <salimsupermario@hotmail.com>
2024-03-13 12:07:47 +0100

    initial-commit

---
+++ Unit_Price.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Load your dataset
+df = pd.read_csv('Price_OS.csv')
+
+# Explore the dataset
+print(df.info())
+print(df.head())
+
+unique_activites = df['ACTIVITE'].unique()
+print(unique_activites)
+
+# Example: Simple random sample for each activite
+sample_size = 30
+random_samples = df.groupby('ACTIVITE').apply(lambda x: x.sample(sample_size)).reset_index(drop=True)
+
+descriptive_stats = random_samples.groupby('ACTIVITE')['prix_unitaire'].agg(['mean', 'median', lambda x: x.mode().iloc[0]]).reset_index()
+descriptive_stats.columns = ['ACTIVITE', 'mean', 'median', 'mode']
+
+print(descriptive_stats)
